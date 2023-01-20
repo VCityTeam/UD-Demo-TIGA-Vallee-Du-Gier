c932f942b133077c75032b4d114a2639245522e0
Lorenzo Marnat <lmarnat14@gmail.com>
2023-01-20 15:42:43 +0100

    Use UD-Viz release

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -38,33 +38,9 @@ Promise.all(list).then(function () {
     );
     app.addModuleView('cityObjects', cityObjectModule.view);
 
-    ////// 3DTILES DEBUG
-    const debug3dTilesWindow = new udviz.Widgets.Debug3DTilesWindow(
-      app.view3D.layerManager
-    );
-    app.addModuleView('3dtilesDebug', debug3dTilesWindow, {
-      name: '3DTiles Debug',
-    });
-
-    ////// CAMERA POSITIONER
-    const cameraPosition = new udviz.Widgets.CameraPositionerView(
-      app.view3D.getItownsView(),
-      app.view3D.getItownsView().controls
-    );
-    app.addModuleView('cameraPositioner', cameraPosition);
-
     ////// LAYER CHOICE MODULE
     const layerChoice = new udviz.Widgets.LayerChoice(app.view3D.layerManager);
     app.addModuleView('layerChoice', layerChoice);
-
-    ////// BaseMap Widget
-    const baseMap = new udviz.Widgets.BaseMap(
-      app.view3D.getItownsView(),
-      config['baseMapLayers'],
-      app.extent,
-      app.config['projection']
-    );
-    app.addModuleView('baseMap', baseMap);
 
     addLabelLayers(config, app.view3D.getItownsView());
 
